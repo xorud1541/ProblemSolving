e4af6f577e03d27e2d668decc099ef4b23e0b65d
전태경 <xorud1544@naver.com>
2019-10-15 23:59:41 +0900

    kakao 2020 1번 푸는중

--- source.cpp
+++ source.cpp
@@ -1,9 +1,88 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+int solution(string s) {
+	int answer = 0;
+
+	for (int bundle = 1; bundle <= s.size() / 2; bundle++)
+	{
+		int index = 0;
+		int cntPattern = 1;
+		string ret;
+		string pattern;
+
+		while (index < s.size())
+		{
+			if (pattern.empty())
+			{
+				//set pattern
+				pattern = s.substr(index, bundle);
+				cntPattern = 1;
+				if (index + bundle >= s.size())
+				{
+					ret += pattern;
+
+					int cnt = s.size() - index - 1;
+					ret += s.substr(index, cnt);
+
+					pattern.clear();
+				}
+				else
+				{
+					index += bundle;
+				}
+				continue;
+			}
+			else
+			{
+				//compare with pattern
+				string temp = s.substr(index, bundle);
+				if (temp == pattern)
+				{
+					cntPattern++;
+					if (index + bundle >= s.size())
+					{
+						ret += to_string(cntPattern) + pattern;
+
+						int cnt = s.size() - index - 1;
+						ret += s.substr(index, cnt);
+
+						pattern.clear();
+					}
+					else
+					{
+						index += bundle;
+					}
+				}
+				else
+				{
+					if (cntPattern != 1)
+						ret += to_string(cntPattern) + pattern;
+					else
+						ret += pattern;
+					pattern.clear();
+				}
+			}
+		}
+
+		if (answer == 0)
+			answer = ret.size();
+		else
+		{
+			if (answer > ret.size())
+				answer = ret.size();
+		}
+
+		ret.clear();
+	}
+	return answer;
+}
+
 int main()
 {
-	cout << "TEST" << endl;
+	cout << solution("aabbaccc") << endl;
 	return 0;
 }
