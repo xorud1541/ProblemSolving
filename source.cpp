ada6748ad4dc4b7967e21e767d1a75bdaaefd221
전태경 <xorud1544@naver.com>
2019-12-05 00:21:24 +0900

    1/100

--- source.cpp
+++ source.cpp
@@ -1,172 +1,114 @@
 #include <iostream>
 #include <vector>
-#define MAX		100
+#include <algorithm>
+#define MAX		500
 using namespace std;
 
-int R, C, M;
+int N, M;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
+bool visited[MAX + 1][MAX + 1];
 
-struct Shark
+int except(int x, int y, vector< vector<int> >& map)
 {
-	Shark(int r, int c, int s, int d, int z)
-		: x(r), y(c), vel(s), dir(d), size(z)
-	{};
-
-	~Shark()
-	{};
-
-	void Up(int dist)
+	int ret = 0;
+	int center = map[x][y];
+	if (x - 1 >= 0 && x + 1 < N)
 	{
-		if (dist > x - 1)
+		int up = map[x - 1][y];
+		int down = map[x + 1][y];
+
+		if (y - 1 >= 0)
 		{
-			dir = 2; //down
-			dist = dist - x + 1;
-			x = 1;
-			Down(dist);
+			int temp = center + up + down + map[x][y - 1];
+			ret = max(ret, temp);
 		}
-		else
-			x = x - dist;
-	}
 
-	void Down(int dist)
-	{
-		if (dist > R - x)
+		if (y + 1 < M)
 		{
-			dir = 1;
-			dist = dist - R + x;
-			x = R;
-			Up(dist);
-		}
-		else
-			x = x + dist;
-	}
-
-	void Left(int dist)
-	{
-		if (dist > y - 1)
-		{
-			dir = 3;
-			dist = dist - y + 1;
-			y = 1;
-			Right(dist);
-		}
-		else
-			y = y - dist;
-	}
-
-	void Right(int dist)
-	{
-		if (dist > C - y)
-		{
-			dir = 4;
-			dist = dist - C + y;
-			y = C;
-			Left(dist);
-		}
-		else
-			y = y + dist;
-	}
-
-	void Move()
-	{
-		switch (dir)
-		{
-		case 1:
-			Up(vel);
-			break;
-		case 2:
-			Down(vel);
-			break;
-		case 3:
-			Right(vel);
-			break;
-		case 4:
-			Left(vel);
-			break;
+			int temp = center + up + down + map[x][y + 1];
+			ret = max(ret, temp);
 		}
 	}
-	
-	pair<int, int> getPos() const { return make_pair(x, y); }
 
-	int getSize() const { return size; }
+	if (y - 1 >= 0 && y + 1 < M)
+	{
+		int left = map[x][y - 1];
+		int right = map[x][y + 1];
 
-	int x;
-	int y;
-	int vel;
-	int dir;
-	int size;
-};
+		if (x - 1 >= 0)
+		{
+			int temp = center + left + right + map[x - 1][y];
+			ret = max(ret, temp);
+		}
+
+		if (x + 1 < N)
+		{
+			int temp = center + left + right + map[x + 1][y];
+			ret = max(ret, temp);
+		}
+	}
+
+	return ret;
+}
+
+int solution(int x, int y, int cnt, vector< vector<int> >& map)
+{
+	int ret = map[x][y];
+	if (cnt == 4)
+	{
+		return ret;
+	}
+
+	int ans = 0;
+	for (int k = 0; k < 4; k++)
+	{
+		int nx = x + dx[k];
+		int ny = y + dy[k];
+
+		if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+
+		if (!visited[nx][ny])
+		{
+			visited[nx][ny] = true;
+
+			int temp = solution(nx, ny, cnt + 1, map);
+
+			ans = max(ans, ret + temp);
+
+			visited[nx][ny] = false;
+		}
+		
+	}
+
+	return ans;
+}
+
 int main()
 {
-	cin >> R >> C >> M;
+	cin >> N >> M;
+
+	vector< vector<int> > map(N, vector<int>(M, 0));
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> map[i][j];
+		}
+	}
+
 	int ans = 0;
-	vector<Shark> sharks;
-	if (M == 0)
+	for (int i = 0; i < N; i++)
 	{
-		cout << ans << endl;
-		return 0;
-	}
-
-	for (int i = 0; i < M; i++)
-	{
-		int r, c, s, d, z;
-		cin >> r >> c >> s >> d >> z;
-		Shark shark(r, c, s, d, z);
-		sharks.push_back(shark);
-	}
-
-	for (int person = 1; person <= C; person++)
-	{
-		int depth = R + 1;
-		bool getShark = false;
-		vector<Shark>::iterator iter, target;
-		for (iter = sharks.begin(); iter != sharks.end(); ++iter)
+		for (int j = 0; j < M; j++)
 		{
-			pair<int, int> pos = iter->getPos();
-			if (pos.second == person)
-			{
-				//사람이랑 같은 곳에 있을 경우
-				
-				if (depth > pos.first)
-				{
-					depth = pos.first;
-					target = iter;
-					getShark = true;
-				}
-			}
-		}
+			visited[i][j] = true;
+			int ret = solution(i, j, 1, map);
+			ans = max(ret, ans);
+			visited[i][j] = false;
 
-		if (getShark)
-		{
-			ans = ans + target->getSize();
-			sharks.erase(target);
-		}
-
-		vector< vector<Shark>::iterator > eraseList;
-		vector< vector< vector<Shark>::iterator > >map(R + 1, vector< vector<Shark>::iterator >(C + 1, sharks.end()));
-		for (iter = sharks.begin(); iter != sharks.end(); ++iter)
-		{
-			iter->Move();
-			pair<int, int> pos = iter->getPos();
-			if (map[pos.first][pos.second] == sharks.end())
-			{
-				map[pos.first][pos.second] = iter;
-			}
-			else
-			{
-				vector<Shark>::iterator other = map[pos.first][pos.second];
-				if (other->getSize() < iter->getSize())
-				{
-					map[pos.first][pos.second] = iter;
-					eraseList.push_back(other);
-				}
-				else
-					eraseList.push_back(iter);
-			}
-		}
-
-		for (auto it : eraseList)
-		{
-			sharks.erase(it);
+			ret = except(i, j, map);
+			ans = max(ans, ret);
 		}
 	}
 
