8c4965d6fda12cbc9edd3da5c859e9e58acde53e
전태경 <xorud1544@naver.com>
2019-12-29 21:31:51 +0900

    백준 2146 느낀점 : 인덱스

--- source.cpp
+++ source.cpp
@@ -1,114 +1,147 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <queue>
 #include <map>
+#include <algorithm>
+#define MAX		101
 using namespace std;
 
-map<string, int> m;
+int N;
+int map_[MAX][MAX];
+bool visited[MAX][MAX];
+
+map<pair<int, int>, int> m;
 int dx[] = { -1, 0, 1, 0 };
 int dy[] = { 0, -1, 0, 1 };
+int cnt = 0;
+int ans = 100000007;
 
-string map_string(vector< vector<int> >& map)
+void bfs(int i, int j)
 {
-	string s;
+	queue<pair<int, int>> q;
+	q.push(make_pair(i, j));
+	visited[i][j] = true;
+	m[make_pair(i, j)] = cnt;
 
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			s += map[i][j] + '0';
-		}
-	}
-	return s;
-}
-
-void string_map(const string& s, vector< vector<int> >& map)
-{
-	map[0][0] = s[0] - '0'; map[0][1] = s[1] - '0'; map[0][2] = s[2] - '0';
-	map[1][0] = s[3] - '0'; map[1][1] = s[4] - '0'; map[1][2] = s[5] - '0';
-	map[2][0] = s[6] - '0'; map[2][1] = s[7] - '0'; map[2][2] = s[8] - '0';
-}
-
-pair<int, int> get_zero(vector< vector<int> >& map)
-{
-	pair<int, int> p;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (map[i][j] == 0)
-			{
-				p = make_pair(i, j);
-				break;
-			}
-		}
-	}
-	return p;
-}
-
-int main()
-{
-	string ans = "123456780";
-
-	vector< vector<int> > map(3, vector<int>(3, 0));
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
-
-	string t = map_string(map);
-	m[t] = 0;
-
-	queue<string> q;
-	q.push(t);
-	bool ok = false;
 	while (!q.empty())
 	{
-		string str = q.front(); q.pop();
-		int cnt = m[str];
-
-		vector< vector<int> > temp_map(3, vector<int>(3, 0));
-		string_map(str, temp_map);
-
-		pair<int, int> p = get_zero(temp_map);
-		int x = p.first;
-		int y = p.second;
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
 
 		for (int k = 0; k < 4; k++)
 		{
 			int nx = x + dx[k];
 			int ny = y + dy[k];
-			if (nx < 0 || nx >= 3 || ny < 0 || ny >= 3) continue;
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
 
-			swap(temp_map[x][y], temp_map[nx][ny]);
-
-			string s = map_string(temp_map);
-			if (m.count(s) == 0)
+			if (map_[nx][ny] == 0)
 			{
-				m[s] = cnt + 1;
-				if (s == ans)
-				{
-					ok = true;
-					break;
-				}
-				q.push(s);
+				//m.insert(make_pair(make_pair(nx, ny), cnt));
+				map_[x][y] = 2;
 			}
 
-			swap(temp_map[x][y], temp_map[nx][ny]);
+			if (!visited[nx][ny])
+			{
+				visited[nx][ny] = true;
+				if (map_[nx][ny] == 1)
+				{
+					q.push(make_pair(nx, ny));
+					m.insert(make_pair(make_pair(nx, ny), cnt));
+				}
+				else {}
+			}
 		}
+	}
+}
 
-		if (ok)
-			break;
+void dfs(int x, int y, int len, int num)
+{
+	if (len > ans)
+	{
+		return;
 	}
 
-	if (m.count(ans) != 0)
-		cout << m[ans] << endl;
-	else
-		cout << -1 << endl;
+	visited[x][y] = true;
 
+	for (int k = 0; k < 4; k++)
+	{
+		int nx = x + dx[k];
+		int ny = y + dy[k];
+		if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+		if (!visited[nx][ny] && map_[nx][ny] != 1)
+		{
+			if (map_[nx][ny] == 0)
+			{
+				visited[nx][ny] = true;
+				dfs(nx, ny, len + 1, num);
+			}
+			else if(map_[nx][ny] == 2)// 2
+			{
+				int n = m[make_pair(nx, ny)];
+				if (n != num)
+				{
+					ans = min(ans, len);
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> map_[i][j];
+		}
+	}
+
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (!visited[i][j] && map_[i][j] == 1)
+			{
+				bfs(i, j);
+				cnt++;
+			}
+		}
+	}
+	/*
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << map_[i][j];
+		}
+		cout << '\n';
+	}
+	*/
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (map_[i][j] == 2)
+			{
+				for (int i = 0; i < N; i++)
+				{
+					for (int j = 0; j < N; j++)
+					{
+						visited[i][j] = false;
+					}
+				}
+
+				int num = m[make_pair(i, j)];
+				dfs(i, j, 0, num);
+			}
+		}
+	}
+
+	cout << ans << endl;
 	return 0;
 }
