393f4e941c2295769d663d25657b0e84bd079316
전태경 <xorud1544@naver.com>
2019-12-30 17:59:04 +0900

    백준 5620 느낀점 : 분할정복 시간복잡도에 대해 다시 복습

--- source.cpp
+++ source.cpp
@@ -1,88 +1,111 @@
 #include <iostream>
-#include <queue>
-#include <map>
-#define MAX		 100001
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-map<pair<int, int>, int> m;
-int a, b, c, d;
-int ans = 100000007;
-int main()
+int N;
+
+typedef struct pos
 {
-	cin >> a >> b >> c >> d;
+	int x;
+	int y;
+} Pos;
 
-	queue<pair<int, int>> q;
-	q.push(make_pair(0, 0));
-	m.insert(make_pair(make_pair(0, 0), 0));
-	bool ok = false;
-	int space = 0;
-	while (!q.empty())
+bool compare(Pos& a, Pos& b)
+{
+	return a.x < b.x;
+}
+
+bool compare2(Pos& a, Pos& b)
+{
+	if (a.y != b.y)
+		return a.y < b.y;
+}
+
+vector< Pos > dots;
+
+int get_distance(const Pos& a, const Pos& b)
+{
+	return ((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y));
+}
+
+int search(int low, int high)
+{
+	int ret = 0;
+	int cnt = high - low + 1;
+
+	if (cnt == 2)
 	{
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-		int cnt = m[make_pair(x, y)];
+		ret = get_distance(dots[low], dots[high]);
+	}
+	else if (cnt == 3)
+	{
+		int mid = low + 1;
+		int x = get_distance(dots[low], dots[high]);
+		int y = get_distance(dots[low], dots[mid]);
+		int z = get_distance(dots[mid], dots[high]);
 
-		for (int k = 0; k < 6; k++)
+		ret = min({ x, y, z });
+	}
+	else
+	{
+		int left = 0;
+		int right = 0;
+		int mid = (low + high) / 2;
+		left = search(low, mid);
+		right = search(mid + 1, high);
+
+		int len = min(left, right);
+
+		int x = dots[mid].x;
+
+		vector<Pos> dots2;
+		for (int i = low; i <= high; i++)
 		{
-			int nx, ny;
-			switch (k)
-			{
-			case 0: // F(A)
-				nx = a; ny = y;
-				break;
-			case 1: // F(B)
-				nx = x; ny = b;
-				break;
-			case 2: // E(A)
-				nx = 0; ny = y;
-				break;
-			case 3: // E(B)
-				nx = x; ny = 0;
-				break;
-			case 4: // M(A, B)
-				space = b - y;
-				if (space >= x)
-				{
-					nx = 0; ny = y + x;
-				}
-				else
-				{
-					nx = x - space; ny = b;
-				}
-				break;
-			case 5:
-				space = a - x;
-				if (space >= y)
-				{
-					nx = x + y; ny = 0;
-				}
-				else
-				{
-					nx = a; ny = y - space;
-				}
-				break;
-			}
+			int diff = x - dots[i].x;
+			if (diff * diff < len)
+				dots2.push_back(dots[i]);
+		}
 
-			if (m.count(make_pair(nx, ny)) == 0)
-			{
-				m.insert(make_pair(make_pair(nx, ny), cnt + 1));
+		sort(dots2.begin(), dots2.end(), compare2);
 
-				if (nx == c && ny == d)
+		for (int i = 0; i < dots2.size(); i++)
+		{
+			for (int j = i + 1; j < dots2.size(); j++)
+			{
+				if ((dots2[j].y - dots2[i].y) * (dots2[j].y - dots2[i].y) < len)
 				{
-					ok = true;
-					break;
+					if (ret == 0)
+					{
+						ret = get_distance(dots2[i], dots2[j]);
+					}
+					else
+					{
+						ret = min(ret, get_distance(dots2[i], dots2[j]));
+					}
 				}
-				q.push(make_pair(nx, ny));
 			}
 		}
-		if (ok)
-			break;
+	}
+	return ret;
+}
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		Pos p;
+		cin >> p.x >> p.y;
+		dots.push_back(p);
 	}
 
-	if (m.count(make_pair(c, d)) != 0)
-		cout << m[make_pair(c, d)] << endl;
-	else
-		cout << -1 << endl;
+	sort(dots.begin(), dots.end(), compare);
+
+	int low = 0;
+	int high = dots.size() - 1;
+
+	int ret = search(low, high);
+
+	cout << ret << endl;
 	return 0;
 }
