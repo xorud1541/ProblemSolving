bd2c9b01ceba898694d93c8379f7579a6907aa31
전태경 <xorud1544@naver.com>
2020-01-01 21:35:04 +0900

    백준 13305 느낀점: 처음엔 쉽게 생각하기

--- source.cpp
+++ source.cpp
@@ -1,113 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <string>
-#include <tuple>
-#define MAX		100000007
 using namespace std;
 
-int N, M, k;
-int dp[1001][1001][12];
-int map[1001][1001];
-
-int dx[] = { -1, 0, 1, 0 };
-int dy[] = { 0, -1, 0, 1 };
+int N;
 int main()
 {
-	cin >> N >> M >> k;
-	vector<string> t;
-	for (int i = 0; i < N; i++)
+	cin >> N;
+	vector< long long > pays(N, 0);
+	vector< long long > line(N - 1, 0);
+
+	for (int i = 0; i < N - 1; i++)
 	{
-		string s;
-		cin >> s;
-		t.push_back(s);
+		cin >> line[i];
 	}
 
 	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < M; j++)
-		{
-			for (int l = 0; l <= k; l++)
-			{
-				dp[i][j][l] = MAX;
-			}
-		}
+		cin >> pays[i];
 	}
 
-	for (int i = 0; i < N; i++)
+	long long min = pays[0];
+	long long ans = 0;
+
+	for (int i = 0; i < N-1; i++)
 	{
-		for (int j = 0; j < M; j++)
-		{
-			map[i][j] = t[i][j] - '0';
-		}
+		long long len = line[i];
+		long long pay = pays[i];
+		if (min > pay)
+			min = pay;
+
+		ans = ans + min * len;
 	}
 
-	queue<tuple<int, int, int>> q;
-	q.push(make_tuple(0, 0, 0));
-	dp[0][0][0] = 1;
-
-	while (!q.empty())
-	{
-		int x, y, z;
-		tie(x, y, z) = q.front(); q.pop();
-
-		for (int d = 0; d < 4; d++)
-		{
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-
-			if (z == k)
-			{
-				if (map[nx][ny] == 0 && (dp[nx][ny][z] > dp[x][y][z] + 1))
-				{
-					dp[nx][ny][z] = dp[x][y][z] + 1;
-					q.push(make_tuple(nx, ny, z));
-				}
-			}
-			else if (z < k)
-			{
-				if (map[nx][ny] == 0 && (dp[nx][ny][z] > dp[x][y][z] + 1))
-				{
-					dp[nx][ny][z] = dp[x][y][z] + 1;
-					q.push(make_tuple(nx, ny, z));
-				}
-				else if (map[nx][ny] == 1 && (dp[nx][ny][z + 1] > dp[x][y][z] + 1))
-				{
-					dp[nx][ny][z + 1] = dp[x][y][z] + 1;
-					q.push(make_tuple(nx, ny, z + 1));
-				}
-			}
-		}
-	}
-
-	bool no = true;
-	for (int i = 0; i <= k; i++)
-	{
-		if (dp[N - 1][M - 1][i] != MAX)
-		{
-			no = false;
-			break;
-		}
-	}
-
-	if (no)
-		cout << -1 << endl;
-	else
-	{
-		int ret = 0;
-		for (int i = 0; i <= k; i++)
-		{
-			if (ret == 0)
-				ret = dp[N - 1][M - 1][i];
-			else
-			{
-				if (ret > dp[N - 1][M - 1][i])
-					ret = dp[N - 1][M - 1][i];
-			}
-		}
-
-		cout << ret << endl;
-	}
+	cout << ans << endl;
 	return 0;
 }
