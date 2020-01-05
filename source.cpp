6ab3b2ace1445f17c27390a4bbeb7ed820b37fe8
전태경 <xorud1544@naver.com>
2020-01-05 18:23:38 +0900

    알고스팟 JUMPGAME 느낀점: 시간초과의 이유 분석

--- source.cpp
+++ source.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <cstring>
+#define MAX	101
 using namespace std;
 
-string solution(string::iterator& iter)
+int cache[MAX][MAX];
+int map[MAX][MAX];
+int N;
+bool go(int i, int j)
 {
-	string out;
-	if (*iter == 'b')
+	if (i >= N || j >= N)
 	{
-		out += 'b';
-	}
-	else if (*iter == 'w')
-	{
-		out += 'w';
-	}
-	else // x
-	{
-		iter++;
-		string a = solution(iter); //b 
-
-		iter++;
-		string b = solution(iter); //b
-
-		iter++;
-		string c = solution(iter); //x
-
-		iter++;
-		string d = solution(iter); //w
-
-		out = 'x' + c + d + a + b;
+		return 0;
 	}
 
-	return out;
+	if (i == N - 1 && j == N - 1)
+	{
+		return 1;
+	}
+
+	int& ret = cache[i][j];
+	if (ret != -1)
+		return ret;
+
+	int jump = map[i][j];
+	int x = i + jump;
+	int y = j + jump;
+
+	return ret = go(x, j) || go(i, y);
 }
 
 int main()
@@ -40,11 +36,23 @@ int main()
 	cin >> C;
 	for (int tc = 1; tc <= C; tc++)
 	{
-		string s;
-		cin >> s;
-		string::iterator iter = s.begin();
-		string out = solution(iter);
-		cout << out << endl;
+		memset(cache, -1, sizeof(cache));
+		//memset(map, 0, sizeof(map));
+		cin >> N;
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				scanf("%d", &map[i][j]);
+			}
+		}
+
+		if (go(0, 0))
+			cout << "YES" << '\n';
+		else
+			cout << "NO" << '\n';
 	}
+
 	return 0;
 }
