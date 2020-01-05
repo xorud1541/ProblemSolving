0ca6b8df81e0f18e17acf3e962bb394145bad763
전태경 <xorud1544@naver.com>
2020-01-05 17:54:07 +0900

    알고스팟 QUADTREE 느낀점: 분할정복은 divide -> merge 다.

--- source.cpp
+++ source.cpp
@@ -1,57 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#define MAX		10
+#include <string>
 using namespace std;
 
-int C, n, m;
+string solution(string::iterator& iter)
+{
+	string out;
+	if (*iter == 'b')
+	{
+		out += 'b';
+	}
+	else if (*iter == 'w')
+	{
+		out += 'w';
+	}
+	else // x
+	{
+		iter++;
+		string a = solution(iter); //b 
+
+		iter++;
+		string b = solution(iter); //b
+
+		iter++;
+		string c = solution(iter); //x
+
+		iter++;
+		string d = solution(iter); //w
+
+		out = 'x' + c + d + a + b;
+	}
+
+	return out;
+}
+
 int main()
 {
+	int C;
 	cin >> C;
 	for (int tc = 1; tc <= C; tc++)
 	{
-		int ans = 0;
-		cin >> n >> m;
-
-		vector<int> f;
-		vector< vector<bool> > isFriends(n, vector<bool>(n, false));
-		for (int i = 0; i < n; i++)
-			f.push_back(i);
-
-		for (int i = 0; i < m; i++)
-		{
-			int a, b;
-			cin >> a >> b;
-			isFriends[a][b] = isFriends[b][a] = true;
-		}
-
-		do
-		{
-			int before = -1;
-			int cur = 0;
-			bool ok = true;
-			for (int i = 0; i < f.size(); i = i + 2)
-			{
-				cur = f[i];
-				int next = f[i + 1];
-				if (before < cur && cur < next && isFriends[cur][next])
-				{
-					before = cur;
-				}
-				else
-				{
-					ok = false;
-					break;
-				}
-			}
-
-			if (ok)
-				ans++;
-
-		} while (next_permutation(f.begin(), f.end()));
-
-		cout << ans << endl;
+		string s;
+		cin >> s;
+		string::iterator iter = s.begin();
+		string out = solution(iter);
+		cout << out << endl;
 	}
-
 	return 0;
 }
