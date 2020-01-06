9f011badbe49d29f1e18b6b2136fe8062f33dcd9
전태경 <xorud1544@naver.com>
2020-01-06 23:23:01 +0900

    알고스팟 STRJOIN

--- source.cpp
+++ source.cpp
@@ -1,51 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
-#define MAX		501
+#define MAX		101
 using namespace std;
-
-int cache[MAX];
-
-int go(int x, vector<int>& arr)
-{
-	if (x == arr.size() - 1)
-		return 1;
-
-	int& ret = cache[x];
-	if (ret != 1)
-		return ret;
-
-	for (int i = x + 1; i < arr.size(); i++)
-	{
-		int next = arr[i];
-		if (next > arr[x])
-		{
-			ret = max(ret, go(i, arr) + 1);
-		}
-	}
-
-	return ret;
-}
 
 int main()
 {
-	int C, N;
+	int C;
 	cin >> C;
 	for (int tc = 1; tc <= C; tc++)
 	{
+		int N;
 		cin >> N;
-		vector<int> arr(N, 0);
+		priority_queue<int, vector<int>, greater<int>> q;
+		
 		for (int i = 0; i < N; i++)
 		{
-			cache[i] = 1;
-			cin >> arr[i];
+			int num;
+			cin >> num;
+			q.push(num);
 		}
 
 		int ans = 0;
-		for (int i = 0; i < N; i++)
+		while (q.size() > 2)
 		{
-			ans = max(ans, go(i, arr));
+			int a = q.top(); q.pop();
+			int b = q.top(); q.pop();
+			ans += a + b;
+
+			q.push(a + b);
 		}
+
+		int a = q.top(); q.pop();
+		int b = q.top(); q.pop();
+
+		ans += a + b;
 
 		cout << ans << endl;
 	}
