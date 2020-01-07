a875480ac8781aef7e29f5ffbe1c82eb21c09453
전태경 <xorud1544@naver.com>
2020-01-07 23:51:41 +0900

    소인수 분해 알고리즘

--- source.cpp
+++ source.cpp
@@ -1,27 +1,18 @@
-// O(루트(n)) 시간에 동작하는 소수 판별 알고리즘
-#include <math.h>
-#include <iostream>
+// 소인수 분해 알고리즘
+#include <vector>
 
 using namespace std;
 
-bool isPrime(int n)
+vector<int> factorSimple(int n)
 {
-	if (n <= 1) return false;
-	if (n == 2) return true;
-
-	if (n % 2 == 0) return false;
-
+	vector<int> ret;
 	int sqrtn = int(sqrt(n));
-	for (int div = 3; div <= sqrtn; div += 2)
-		if (n % div == 0)
-			return false;
-	return true;
-}
-
-int main()
-{
-	cout << isPrime(5) << endl;
-	cout << isPrime(6) << endl;
-	cout << isPrime(13) << endl;
-	return 0;
+	for(int div = 2; div <= sqrtn; ++div)
+		while (n % div == 0)
+		{
+			n /= div;
+			ret.push_back(div);
+		}
+	if (n > 1) ret.push_back(n);
+	return ret;
 }
