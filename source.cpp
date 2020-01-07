3a1437ae5879ee20d220116013263e463feb3926
전태경 <xorud1544@naver.com>
2020-01-07 23:47:42 +0900

    소수 판별 알고리즘

--- source.cpp
+++ source.cpp
@@ -1,43 +1,27 @@
+// O(루트(n)) 시간에 동작하는 소수 판별 알고리즘
+#include <math.h>
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#define MAX		101
+
 using namespace std;
+
+bool isPrime(int n)
+{
+	if (n <= 1) return false;
+	if (n == 2) return true;
+
+	if (n % 2 == 0) return false;
+
+	int sqrtn = int(sqrt(n));
+	for (int div = 3; div <= sqrtn; div += 2)
+		if (n % div == 0)
+			return false;
+	return true;
+}
 
 int main()
 {
-	int C;
-	cin >> C;
-	for (int tc = 1; tc <= C; tc++)
-	{
-		int N;
-		cin >> N;
-		priority_queue<int, vector<int>, greater<int>> q;
-		
-		for (int i = 0; i < N; i++)
-		{
-			int num;
-			cin >> num;
-			q.push(num);
-		}
-
-		int ans = 0;
-		while (q.size() > 2)
-		{
-			int a = q.top(); q.pop();
-			int b = q.top(); q.pop();
-			ans += a + b;
-
-			q.push(a + b);
-		}
-
-		int a = q.top(); q.pop();
-		int b = q.top(); q.pop();
-
-		ans += a + b;
-
-		cout << ans << endl;
-	}
+	cout << isPrime(5) << endl;
+	cout << isPrime(6) << endl;
+	cout << isPrime(13) << endl;
 	return 0;
 }
