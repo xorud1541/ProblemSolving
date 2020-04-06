e659508f014d57f096bc5801cf6efb6b5e193352
전태경 <xorud1544@naver.com>
2020-04-07 00:04:22 +0900

    codeforce #630 A.Exercising Walk

--- source.cpp
+++ source.cpp
@@ -1,153 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#define MAX		12
 using namespace std;
 
-bool isAnswer(vector< vector<int> >& arr, int n)
-{
-	bool ok = true;
-	if (arr[3].empty())
-		return false;
-
-	for (int i = 1; i <= n; i++)
-	{
-		if (arr[3][i - 1] != i)
-		{
-			ok = false;
-			break;
-		}
-	}
-	return ok;
-}
-
-bool isMove(vector<int>& src, vector<int>& dst)
-{
-	if (!src.empty())
-	{
-		int src_back = src.back();
-		int dst_back;
-
-		if (dst.empty())
-		{
-			src.pop_back();
-			dst.push_back(src_back);
-		}
-		else
-		{
-			dst_back = dst.back();
-			if (dst_back > src_back)
-			{
-				src.pop_back();
-				dst.push_back(src_back);
-			}
-			else
-				return false;
-		}
-
-		return true;
-	}
-	else
-		return false;
-}
-
+int T;
 int main()
 {
-	int C;
-	cin >> C;
-	for (int tc = 1; tc <= C; tc++)
+	cin >> T;
+	while (T--)
 	{
-		int N;
-		cin >> N;
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
 
-		vector< vector<int> > arr;
-		for (int i = 0; i < 4; i++)
-		{
-			int num;
-			cin >> num;
-			vector<int> a(num, 0);
-			for (int j = 0; j < num; j++)
-			{
-				cin >> a[j];
-			}
-			arr.push_back(a);
-		}
+		int x, y, x1, y1, x2, y2;
+		cin >> x >> y >> x1 >> y1 >> x2 >> y2;
 
-		queue< pair<vector< vector<int> >, int> > q;
-
-		int ans = 0;
-		bool end = false;
-		if (isAnswer(arr, N))
-		{
-			cout << ans << endl;
-			return 0;
-		}
+		if (x1 == x2 && (a != 0 || b != 0))
+			cout << "NO" << endl;
+		else if (y1 == y2 && (c != 0 || d != 0))
+			cout << "NO" << endl;
 		else
 		{
-			q.push(make_pair(arr, 0));
-
-			while (!q.empty())
-			{
-				vector< vector<int> > temp = q.front().first;
-				int cnt = q.front().second;
-				q.pop();
-
-				for (int k = 0; k < 4; k++)
-				{
-					if (isMove(temp[k], temp[(k + 1) % 4]))
-					{
-						if (isAnswer(temp, N))
-						{
-							ans = cnt + 1;
-							end = true;
-						}
-						else
-						{
-							int back = temp[(k + 1) % 4].back();
-							temp[k].push_back(back);
-							temp[(k + 1) % 4].pop_back();
-							q.push(make_pair(temp, cnt + 1));
-						}
-					}
-
-					if (isMove(temp[k], temp[(k + 2) % 4]))
-					{
-						if (isAnswer(temp, N))
-						{
-							ans = cnt + 1;
-							end = true;
-						}
-						else
-						{
-							int back = temp[(k + 2) % 4].back();
-							temp[k].push_back(back);
-							temp[(k + 2) % 4].pop_back();
-							q.push(make_pair(temp, cnt + 1));
-						}
-					}
-
-					if (isMove(temp[k], temp[(k + 3) % 4]))
-					{
-						if (isAnswer(temp, N))
-						{
-							ans = cnt + 1;
-							end = true;
-						}
-						else
-						{
-							int back = temp[(k + 3) % 4].back();
-							temp[k].push_back(back);
-							temp[(k + 3) % 4].pop_back();
-							q.push(make_pair(temp, cnt + 1));
-						}
-					}
-				}
-
-				if (end)
-					break;
-			}
-
-			cout << ans << endl;
+			int nx = x - a + b;
+			int ny = y - c + d;
+			if (nx >= x1 && nx <= x2 && ny >= y1 && ny <= y2)
+				cout << "YES" << endl;
+			else
+				cout << "NO" << endl;
 		}
 	}
 	return 0;
