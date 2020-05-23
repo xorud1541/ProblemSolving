31fa6c271a582b48966d5b45af5142917b09fe4f
전태경 <xorud1544@naver.com>
2020-05-23 21:18:57 +0900

    [종만북] #PICNIC

--- source.cpp
+++ source.cpp
@@ -1,31 +1,52 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int T;
 int main()
 {
+	int T;
 	cin >> T;
 	while (T--)
 	{
-		int a, b, c, d;
-		cin >> a >> b >> c >> d;
-
-		int x, y, x1, y1, x2, y2;
-		cin >> x >> y >> x1 >> y1 >> x2 >> y2;
-
-		if (x1 == x2 && (a != 0 || b != 0))
-			cout << "NO" << endl;
-		else if (y1 == y2 && (c != 0 || d != 0))
-			cout << "NO" << endl;
-		else
+		int n, m;
+		cin >> n >> m;
+		vector<vector<bool>> isFriends(n, vector<bool>(n, false));
+		for (int i = 0; i < m; i++)
 		{
-			int nx = x - a + b;
-			int ny = y - c + d;
-			if (nx >= x1 && nx <= x2 && ny >= y1 && ny <= y2)
-				cout << "YES" << endl;
-			else
-				cout << "NO" << endl;
+			int a, b;
+			cin >> a >> b;
+			isFriends[a][b] = true;
+			isFriends[b][a] = true;
 		}
+
+		int ans = 0;
+		vector<int> people;
+		for (int i = 0; i < n; i++)
+			people.push_back(i);
+
+		do
+		{
+			bool ok = true;
+			int before = -1;
+			for (int i = 0; i < n; i = i + 2)
+			{
+				int a = people[i];
+				int b = people[i + 1];
+				if (a < before || a > b || !isFriends[a][b])
+				{
+					ok = false;
+					break;
+				}
+
+				before = a;
+			}
+
+			if (ok)
+				ans++;
+		} while (next_permutation(people.begin(), people.end()));
+
+		cout << ans << endl;
 	}
 	return 0;
 }
