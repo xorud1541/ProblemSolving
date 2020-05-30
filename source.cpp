35ec259d83f74675eccf004b43b7b478990b2cc8
전태경 <xorud1544@naver.com>
2020-05-30 09:44:54 +0900

    [프로그래머스] #종이접기

--- source.cpp
+++ source.cpp
@@ -1,77 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+#include <string>
 using namespace std;
 
-int solution(vector<int>& vec, int left, int right)
+string recursive(int n, int t, char num)
 {
-	int N = right - left + 1;
-	if (N == 1) // base case
+	string ret;
+	if (n == t)
 	{
-		return vec[left];
+		ret += '0';
+		ret += num;
+		ret += '1';
+		return ret;
 	}
 	else
 	{
-		int mid = (right + left) / 2;
-		int ret = max(solution(vec, left, mid), solution(vec, mid + 1, right));
-
-		int l = mid;
-		int r = mid + 1;
-		int h = min(vec[l], vec[r]);
-		int w = 0;
-		while (1)
-		{
-			w = r - l + 1;
-			int range = h * w;
-			ret = max(range, ret);
-
-			if (l > left && r < right)
-			{
-				if (vec[l - 1] < vec[r + 1])
-				{
-					h = min(h, vec[r + 1]);
-					r++;
-				}
-				else
-				{
-					h = min(h, vec[l - 1]);
-					l--;
-				}
-			}
-			else if (l == left && r < right)
-			{
-				h = min(h, vec[r + 1]);
-				r++;
-			}
-			else if (l > left && r == right)
-			{
-				h = min(h, vec[l - 1]);
-				l--;
-			}
-			else
-				break;
-		}
-
+		ret += recursive(n, t + 1, '0');
+		ret += num;
+		ret += recursive(n, t + 1, '1');
 		return ret;
 	}
 }
 
+vector<int> solution(int n) {
+	vector<int> answer;
+
+	string s = recursive(n, 1, '0');
+	for (int i = 0; i < s.size(); i++)
+	{
+		answer.push_back(s[i] - '0');
+	}
+	return answer;
+}
+
 int main()
 {
-	int T;
-	cin >> T;
-	while (T--)
-	{
-		int N;
-		cin >> N;
-		vector<int> vec(N, 0);
-		for (int i = 0; i < N; i++)
-		{
-			cin >> vec[i];
-		}
-
-		cout << solution(vec, 0, N-1) << endl;
-	}
+	solution(3);
 	return 0;
 }
