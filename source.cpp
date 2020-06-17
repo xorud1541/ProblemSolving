f24929dada358caa279d9762af94a95f7176c15e
전태경 <xorud1544@naver.com>
2020-06-17 23:59:40 +0900

    [백준] #부분수열의 합

--- source.cpp
+++ source.cpp
@@ -1,91 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <set>
 #include <algorithm>
 using namespace std;
 
-set<string> s;
-vector< vector<int> > indexs;
-vector<int> ret;
-int taken[9];
-int answer;
-
-bool isSame(string& a, string& b)
-{
-	if (a.size() == b.size())
-	{
-		for (int i = 0; i < a.size(); i++)
-		{
-			if (a[i] != '*' && a[i] != b[i])
-				return false;
-		}
-
-		return true;
-	}
-	else
-		return false;
-}
-
-void dfs(vector<string>& user_id, vector<string>& banned_id) {
-	int len = ret.size();
-
-	if (len == indexs.size())
-	{
-		vector<int> vec = ret;
-		sort(vec.begin(), vec.end());
-		string tmp;
-		for (int i : vec)
-			tmp += i + '0';
-
-		s.insert(tmp);
-		return;
-	}
-
-	
-	vector<int> index = indexs[len];
-	for (int i : index)
-	{
-		if (!taken[i])
-		{
-			taken[i] = true;
-			ret.push_back(i);
-
-			dfs(user_id, banned_id);
-
-			ret.pop_back();
-			taken[i] = false;
-		}
-	}
-}
-
-int solution(vector<string> user_id, vector<string> banned_id) {
-
-	for (int i = 0; i < banned_id.size(); i++)
-	{
-		vector<int> a;
-		for (int j = 0; j < user_id.size(); j++)
-		{
-			if (isSame(banned_id[i], user_id[j]))
-			{
-				a.push_back(j);
-			}
-		}
-		indexs.push_back(a);
-	}
-
-	dfs(user_id, banned_id);
-
-	answer = s.size();
-
-	return answer;
-}
-
 int main()
 {
-	vector<string> user = { "frodo", "fradi", "crodo", "abc123", "frodoc" };
-	vector<string> ban = { "fr*d*", "*rodo", "******", "******" };
+	int N, S, answer = 0;
+	cin >> N >> S;
+	vector<int> arr(N, 0);
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
 
-	int a = solution(user, ban);
+	for (int i = 1; i <= N; i++)
+	{
+		vector<int> indexs(N, 0);
+		for (int j = 0; j < i; j++)
+			indexs[j] = 1;
+
+
+		do
+		{
+			int sum = 0;
+			for (int i = 0; i < N; i++)
+			{
+				if (indexs[i])
+				{
+					sum += arr[i];
+				}
+			}
+
+			if (sum == S)
+				answer++;
+
+		} while (prev_permutation(indexs.begin(), indexs.end()));
+
+	}
+
+	cout << answer << "\n";
 	return 0;
 }
