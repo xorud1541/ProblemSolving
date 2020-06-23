1fce4718a81dd6e71a87de64edc71e197a9b32a2
전태경 <xorud1544@naver.com>
2020-06-24 00:43:03 +0900

    [백준] #영역 구하기

--- source.cpp
+++ source.cpp
@@ -1,48 +1,77 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#define MAX		1001
-
+#include <queue>
 using namespace std;
 
-int cache[MAX];
-int arr[MAX];
-int N;
-int go(int x)
+int M, N, K;
+
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, -1, 0, 1 };
+
+void check(vector< vector<bool> >& map, int x1, int y1, int x2, int y2)
 {
-	if (x == N - 1)
+	for (int i = x1; i <= x2; i++)
 	{
-		return arr[x];
+		for (int j = y1; j <= y2; j++)
+		{
+			map[i][j] = true;
+		}
 	}
+}
 
-	int& ret = cache[x];
-	if (ret != 0)
-		return ret;
-
-	int num = arr[x];
-	ret = num;
-	for (int i = x + 1; i < N; i++)
+int dfs(vector <vector<bool> >& map, int x, int y)
+{
+	int ret = 0;
+	for (int k = 0; k < 4; k++)
 	{
-		if(arr[i] > num)
-			ret = max(ret, num + go(i));
+		int nx = x + dx[k];
+		int ny = y + dy[k];
+		if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
+
+		if (!map[nx][ny])
+		{
+			map[nx][ny] = true;
+			ret += dfs(map, nx, ny) + 1;
+		}
 	}
 	return ret;
 }
 
 int main()
 {
-	cin >> N;
-	
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
+	cin >> M >> N >> K;
+	priority_queue<int, vector<int>, greater<int> > q;
+	vector< vector<bool> > map(M, vector<bool>(N, 0));
 
-	int ans = 0;
-	
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < K; i++)
 	{
-		ans = max(ans, go(i));
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+
+		check(map, y1, x1, y2 - 1, x2 - 1);
 	}
 
-	cout << ans << endl;
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (!map[i][j])
+			{
+				map[i][j] = true;
+				int ret = dfs(map, i, j) + 1;
+				q.push(ret);
+			}
+		}
+	}
+
+	int cnt = q.size();
+	cout << cnt << "\n";
+	while (!q.empty())
+	{
+		int ans = q.top();
+		cout << ans << endl;
+		q.pop();
+	}
+
 	return 0;
 }
