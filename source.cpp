f625ba660b8b491b78aeea46f582275f1f05efd0
전태경 <xorud1544@naver.com>
2020-06-28 23:00:47 +0900

    [백준] #양

--- source.cpp
+++ source.cpp
@@ -1,104 +1,94 @@
 #include <iostream>
+#include <vector>
 #include <cstring>
-#include <algorithm>
 #include <queue>
+#define MAX		251
 
 using namespace std;
+int R, C;
+bool visited[MAX][MAX];
 
-int n, m, Sx = -1, Sy, Ex, Ey;
-char map[1505][1505];
-bool check[1505][1505];
-int p[1505 * 1505];
 int dx[] = { -1, 0, 1, 0 };
 int dy[] = { 0, -1, 0, 1 };
 
-bool inner(int x, int y) {
-	return (0 <= x && x < n && 0 <= y && y < m);
-}
-
-int find(int x)
+pair<int, int> count(int x, int y, vector< vector<char> >& map)
 {
-	if (p[x] == x)
-		return x;
-	return p[x] = find(p[x]);
-}
+	int ans1 = 0;
+	int ans2 = 0;
+	if (map[x][y] == 'v')
+		ans2++;
+	else if (map[x][y] == 'o')
+		ans1++;
+	else {}
 
-void merge(int x, int y)
-{
-	x = find(x);
-	y = find(y);
-	if (x == y)
-		return;
-	p[x] = y;
+	queue<pair<int, int>> q;
+	q.push({ x, y });
+
+	while (!q.empty())
+	{
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+
+		for (int k = 0; k < 4; k++)
+		{
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if (nx < 0 || nx >= R || ny < 0 || ny >= C)
+				continue;
+
+			if (!visited[nx][ny] && map[nx][ny] != '#')
+			{
+				visited[nx][ny] = true;
+
+				if (map[nx][ny] == 'v')
+					ans2++;
+				else if(map[nx][ny] == 'o')
+					ans1++;
+
+				q.push({ nx, ny });
+			}
+		}
+	}
+
+	return make_pair(ans1, ans2);
 }
 
 int main()
 {
-	for (int i = 0; i < 1500 * 1500 + 2; i++)
-		p[i] = i;
+	cin >> R >> C;
+	vector< vector<char> > map(R, vector<char>(C, 0));
 
-	scanf("%d %d", &n, &m);
-	queue<pair<int, int>> q;
-
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < R; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < C; j++)
 		{
-			scanf("%1c", &map[i][j]);
-			if (map[i][j] == 'X')
-				continue;
-			if (map[i][j] == 'L')
-			{
-				if (Sx == -1)
-				{
-					Sx = i;
-					Sy = j;
-				}
-				else
-				{
-					Ex = i;
-					Ey = j;
-				}
-				map[i][j] = '.';
-			}
-
-			q.push({ i, j });
+			cin >> map[i][j];
 		}
 	}
 
-	int ans = 0;
-	while (int s = q.size())
+	int ans1 = 0;
+	int ans2 = 0;
+	for (int i = 0; i < R; i++)
 	{
-		memset(check, 0, sizeof(check));
-		while (s--)
+		for (int j = 0; j < C; j++)
 		{
-			int x = q.front().first;
-			int y = q.front().second;
-			q.pop();
-			for (int i = 0; i < 4; i++)
+			if (!visited[i][j] && map[i][j] != '#')
 			{
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (!inner(nx, ny))
-					continue;
-
-				if (map[nx][ny] == 'X')
+				visited[i][j] = true;
+				pair<int, int> cnt = count(i, j, map);
+				if (cnt.first <= cnt.second)
 				{
-					q.push({ nx, ny });
-					check[nx][ny] = true;
-					map[nx][ny] = '.';
+					ans2 += cnt.second;
 				}
 				else
 				{
-					if (!check[nx][ny])
-						merge(find(x*m + y), find(nx*m + ny));
+					ans1 += cnt.first;
 				}
 			}
 		}
-		if (find(Sx*m + Sy) == find(Ex*m + Ey)) {
-			printf("%d\n", ans);
-			return 0;
-		}
-		ans++;
 	}
+
+	printf("%d %d", ans1, ans2);
+	return 0;
 }
